292b6f0983491d6655ec1ce4e0d3aeaceef4f271
StevenTagawa <42248805+StevenTagawa@users.noreply.github.com>
2020-10-27 04:12:33 -0700

    Work on redirecting to last route after register/login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Main module for the Learning Journal app."""
 
+# Imports.
 from flask import (
     flash,
     Flask,
@@ -22,9 +23,21 @@ import random
 import forms
 import models
 
+
+# Constants.
 DEBUG = True
 PORT = 8000
 HOST = '127.0.0.1'
+
+# Global variables.
+last_route = None
+cur_user = None
+cur_entry = None
+"""Global variables are set by each route, and track both the route and the
+    variable elements of the URL for the route.  These are used by the url_for
+    method in the get_last_route function to build a URL to which the user is
+    redirected after registering or logging in.  
+"""
 
 
 def get_secret_key():
@@ -72,6 +85,10 @@ def index(home=True):
     entries.  If a user is logged in, also displays links for the user's private
     and hidden entries.
     """
+    global last_route, cur_user, cur_entry
+    last_route = "index"
+    cur_user = None
+    cur_entry = None
     if current_user.is_authenticated:
         if current_user.god:
             entries = models.Entry.select().order_by(models.Entry.date.asc())
@@ -87,7 +104,8 @@ def index(home=True):
             god=current_user.god, home=home, by="All")
     else:
         entries = models.Entry.select().where(
-            models.Entry.hidden == False).order_by(models.Entry.date.asc())  # noqa
+            models.Entry.hidden == False).order_by(
+            models.Entry.date.asc())  # noqa E712 (must use == for peewee)
         return render_template("listing.html", entries=entries, user="",
                                god=False, home=home, by="All")
 
@@ -95,6 +113,10 @@ def index(home=True):
 @app.route("/entries")
 def entries():
     """Prevents url_for from returning "/entries" for "index"."""
+    global last_route, cur_user, cur_entry
+    last_route = "entries"
+    cur_user = None
+    cur_entry = None
     index(home=False)
 
 
@@ -107,11 +129,15 @@ def user_entries(user):
     links for all entries.  Otherwise, displays title and date for all non-
     hidden entries, and links for all public entries.
     """
+    global last_route, cur_user, cur_entry
+    last_route = "user_entries"
+    cur_user = user
+    cur_entry = None
     if current_user.is_authenticated and current_user.username == user:
         entries = current_user.entries.order_by(models.Entry.date.asc())
     else:
         entries = (models.Entry.select().where(
-            models.Entry.hidden == False)  # noqa
+            models.Entry.hidden == False)  # noqa E712 (must use == in peewee)
                    .join(User)
                    .where(User.username == user)
                    .order_by(models.Entry.date.asc()))
@@ -121,15 +147,25 @@ def user_entries(user):
 
 @app.route("/register", methods=("GET", "POST"))
 def register():
+    """Creates a new user account."""
     form = forms.RegisterForm()
     if form.validate_on_submit():
-        flash("Registration successful", "success")
-        models.User.create_user(
-            username=form.username.data,
-            password=form.password.data,
-            god=False)
-        login_user(models.User.get(models.User.username == form.username.data))
-        return redirect(url_for("index"))
+        try:
+            models.User.get(models.User.username == form.username.data)
+        except models.DoesNotExist:
+            flash("Registration successful", "success")
+            models.User.create_user(
+                username=form.username.data,
+                password=form.password.data,
+                god=False)
+            flash("Registration successful", "success")
+            # Automatically log in after registration.
+            login_user(models.User.get(
+                models.User.username == form.username.data))
+            return redirect(get_last_route())
+        else:
+            flash("User already exists", "error")
+            return redirect(url_for("login"))
     return render_template("form.html", button="Register", form=form)
 
 
@@ -145,9 +181,7 @@ def login():
             if check_password_hash(user.password, form.password.data):
                 login_user(user)
                 flash("Login successful.", "success")
-                return redirect(
-                    url_for("user_entries",
-                            user=g.user._get_current_object().username))  # noqa
+                return redirect(get_last_route())
             else:
                 flash("Incorrect username or password.", "error")
     return render_template("form.html", button="Log In", form=form)
@@ -164,13 +198,19 @@ def logout():
 @app.route("/entries/new", methods=("GET", "POST"))
 @login_required
 def create_entry():
+    """Creates a new entry."""
+    global last_route, cur_user, cur_entry
+    last_route = "create_entry"
+    cur_user = None
+    cur_entry = None
+    user = g.user._get_current_object()  # noqa (accessing private method)
     form = forms.EntryForm()
     if form.validate_on_submit():
         # All entries marked hidden are also private.
         if form.hidden.data:
             form.private.data = True
         models.Entry.create(
-            user=g.user._get_current_object(),  # noqa
+            user=user,
             title=form.title.data,
             date=form.date.data,
             time_spent=form.time_spent.data,
@@ -191,12 +231,17 @@ def create_entry():
                     tag = models.Tag.create(name=tag)
                 models.EntryTag.create(entry=entry, tag=tag)
         flash("Entry saved.", "success")
-        return redirect(url_for("index"))
+        return redirect(url_for("user_entries", user=user.username))
     return render_template("form.html", button="Create", form=form)
 
 
 @app.route("/entries/<int:entry_id>", methods=("GET", "POST"))
 def show_entry(entry_id):
+    """Displays a single entry, if the user is authorized to view it."""
+    global last_route, cur_user, cur_entry
+    last_route = "show_entry"
+    cur_user = None
+    cur_entry = entry_id
     try:
         entry = models.Entry.get(models.Entry.id == entry_id)
         # Deny that hidden entries exist, except to the author (and god).
@@ -217,6 +262,11 @@ def show_entry(entry_id):
 @app.route("/entries/<int:entry_id>/edit", methods=("GET", "POST"))
 @login_required
 def edit_entry(entry_id):
+    """Allows the user to edit an entry, if they are authorized to do so."""
+    global last_route, cur_user, cur_entry
+    last_route = "edit_entry"
+    cur_user = None
+    cur_entry = entry_id
     try:
         # Entries can only be edited by the author.
         entry = models.Entry.get(models.Entry.id == entry_id)
@@ -234,8 +284,6 @@ def edit_entry(entry_id):
         form.date.data = entry.date
         form.time_spent.data = entry.time_spent
         form.learned.data = entry.learned
-        print("Before edit, form.learned.data = ", form.learned.data)
-        print("Before edit, entry.learned = ", entry.learned)
         form.resources.data = entry.resources
         form.tags.data = entry.tags
         form.private.data = entry.private
@@ -244,8 +292,6 @@ def edit_entry(entry_id):
         entry.title = form.title.data
         entry.date = form.date.data
         entry.time_spent = form.time_spent.data
-        print("After edit, form.learned.data = ", form.learned.data)
-        print("After edit, entry.learned = ", entry.learned)
         entry.learned = form.learned.data
         entry.resources = form.resources.data
         entry.tags = form.tags.data
@@ -272,27 +318,42 @@ def edit_entry(entry_id):
                 tag = models.Tag.create(name=tag)
                 models.EntryTag.create(entry=entry, tag=tag)
         flash("Entry edited.", "success")
-        return redirect(url_for("index"))
+        return redirect(url_for("show_entry", entry_id=entry_id))
     return render_template("form.html", button="Update", form=form)
 
 
 @app.route("/entries/<int:entry_id>/delete", methods=("GET", "POST"))
 @login_required
 def delete_entry(entry_id):
+    """Deletes an entry, if the user is authorized to do so."""
+    global last_route, cur_user, cur_entry
+    last_route = "delete_entry"
+    cur_user = None
+    cur_entry = None
+    user = g.user._get_current_object()  # noqa (accessing private method)
     try:
         # Entries can only be deleted by the author.
         entry = models.Entry.get(models.Entry.id == entry_id)
-        if g.user._get_current_object() != entry.user:  # noqa
+        if user != entry.user:
             raise models.DoesNotExist
     except models.DoesNotExist:
         flash("Cannot delete entry.", "error")
-        return redirect(url_for("index"))
+        return redirect(url_for(last_route))
     # Delete associated tags before deleting the entry.
     models.EntryTag.delete().where(models.EntryTag.entry == entry)
     entry.delete_instance()
     flash("Entry deleted.", "success")
-    return redirect(url_for(
-        "user_entries", user=g.user._get_current_object().username))  # noqa
+    return redirect(url_for("user_entries", user=user.username))
+
+
+def get_last_route(user=cur_user, entry_id=cur_entry):
+    """Returns data for the url_for method based on the last route processed."""
+    if last_route in ["user_entries"]:
+        return url_for(last_route, user=user)
+    elif last_route in ["show_entry", "edit_entry", "delete_entry"]:
+        return url_for(last_route, entry_id=entry_id)
+    else:
+        return url_for(last_route)
 
 
 # EXECUTION BEGINS HERE
